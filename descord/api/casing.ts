a2c2fddb59724f09657fc224bcd31d8e9e67ce3d
A. Matías Quezada <amatiasq@gmail.com>
2020-06-19 01:24:54 +0200

    Casing file

---
+++ descord/api/casing.ts
@@ -0,0 +1,8 @@
+import {
+	renameObjectKeys,
+	camelCaseToSnakeCase,
+	snakeCaseToPascalCase,
+} from '../../amq/casing.ts';
+
+export const toApiCasing = renameObjectKeys(camelCaseToSnakeCase);
+export const fromApiCasing = renameObjectKeys(snakeCaseToPascalCase);
